e43ba8b9aa026b0244b224ca7318f90b71506921
divine016 <koutidivine@gmail.com>
2024-03-07 15:06:49 +0100

    modified some file

--- pesa/src/sections/Functionalities.jsx
+++ pesa/src/sections/Functionalities.jsx
@@ -1,6 +1,17 @@
+import { services } from "../constants"
+import ServiceCard from "../components/ServiceCard"
+
 const Functionalities = () => {
   return (
-    <div>Functionalities</div>
+    <section className="max-container">
+      <h2 className="font-bold text-[50px] text-center font-palanquin leading-normal py-4">Our <span className="text-coral-red">Services</span> </h2>
+      <div className="flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+      <ServiceCard key={service.label} {...service} />
+    ))}
+      </div>
+      
+    </section>
   )
 }
 
